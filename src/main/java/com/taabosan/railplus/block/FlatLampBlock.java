6ee84f9cf6b19bd618d2a4c5dfbfa56593aa7cf0
tabosan <tabosan0916@outlook.jp>
2025-12-30 16:31:56 +0900

    lampを水の中に置けるようにした

--- src/main/java/com/taabosan/railplus/block/FlatLampBlock.java
+++ src/main/java/com/taabosan/railplus/block/FlatLampBlock.java
@@ -3,18 +3,27 @@ package com.taabosan.railplus.block;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class FlatLampBlock extends FaceAttachedHorizontalDirectionalBlock {
+public class FlatLampBlock extends FaceAttachedHorizontalDirectionalBlock implements SimpleWaterloggedBlock {
     public static final MapCodec<FlatLampBlock> CODEC = simpleCodec(FlatLampBlock::new);
+
+    public static final BooleanProperty WATERLOGGED;
 
     private static final VoxelShape FLOOR = Block.box(3, 0, 3, 13, 2, 13);
     private static final VoxelShape CEILING = Block.box(3, 14, 3, 13, 16, 13);
@@ -27,7 +36,7 @@ public class FlatLampBlock extends FaceAttachedHorizontalDirectionalBlock {
 
     public FlatLampBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACE, AttachFace.FLOOR).setValue(FACING, Direction.NORTH));
+        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACE, AttachFace.FLOOR).setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false));
     }
 
     @Override
@@ -48,7 +57,32 @@ public class FlatLampBlock extends FaceAttachedHorizontalDirectionalBlock {
         };
     }
 
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+        return super.updateShape(
+                state, direction, neighborState, level, pos, neighborPos
+        );
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        FluidState fluid = ctx.getLevel().getFluidState(ctx.getClickedPos());
+
+        return super.getStateForPlacement(ctx).setValue(WATERLOGGED, fluid.getType() == Fluids.WATER);
+    }
+
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{FACING, FACE});
+        builder.add(new Property[]{FACING, FACE, WATERLOGGED});
+    }
+
+    protected FluidState getFluidState(BlockState state) {
+        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    static {
+        WATERLOGGED = BlockStateProperties.WATERLOGGED;
     }
 }
--- src/main/java/com/taabosan/railplus/block/SquareLampBlock.java
+++ src/main/java/com/taabosan/railplus/block/SquareLampBlock.java
@@ -3,18 +3,27 @@ package com.taabosan.railplus.block;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class SquareLampBlock extends FaceAttachedHorizontalDirectionalBlock {
+public class SquareLampBlock extends FaceAttachedHorizontalDirectionalBlock implements SimpleWaterloggedBlock {
     public static final MapCodec<SquareLampBlock> CODEC = simpleCodec(SquareLampBlock::new);
+
+    public static final BooleanProperty WATERLOGGED;
 
     private static final VoxelShape FLOOR = Block.box(4, 0, 4, 12, 12, 12);
     private static final VoxelShape CEILING = Block.box(4, 4, 4, 12, 16, 12);
@@ -27,7 +36,7 @@ public class SquareLampBlock extends FaceAttachedHorizontalDirectionalBlock {
 
     public SquareLampBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACE, AttachFace.FLOOR).setValue(FACING, Direction.NORTH));
+        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue(FACE, AttachFace.FLOOR).setValue(FACING, Direction.NORTH).setValue(WATERLOGGED, false));
     }
 
     @Override
@@ -48,7 +57,32 @@ public class SquareLampBlock extends FaceAttachedHorizontalDirectionalBlock {
         };
     }
 
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+        return super.updateShape(
+                state, direction, neighborState, level, pos, neighborPos
+        );
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        FluidState fluid = ctx.getLevel().getFluidState(ctx.getClickedPos());
+
+        return super.getStateForPlacement(ctx).setValue(WATERLOGGED, fluid.getType() == Fluids.WATER);
+    }
+
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{FACING, FACE});
+        builder.add(new Property[]{FACING, FACE, WATERLOGGED});
+    }
+
+    protected FluidState getFluidState(BlockState state) {
+        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    static {
+        WATERLOGGED = BlockStateProperties.WATERLOGGED;
     }
 }
